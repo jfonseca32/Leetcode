52d738c6eae63e5eb5e663f0d339e8e800f4f352
Joao Pedro Dudziak Fonseca <jfonseca2003@gmail.com>
2026-07-28 09:37:04 -0700

    Solve problem + precommit and settings

---
+++ 0001_two_sum/1_two_sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        # Hashmap for O(N) time and space
+        num_to_index = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num # get number to check dict
+
+            if complement in num_to_index: # check if complement in dict
+                return [i, num_to_index[complement]]
+
+            num_to_index[num] = i # add num-index to dict
